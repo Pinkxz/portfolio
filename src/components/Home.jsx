901528104d2fa4d01ef78142b0a630b9f3c6dff3
pinkxz <victor666157@gmail.com>
2025-04-04 18:27:25 -0300

    typed e visualmente mais bonito

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,34 @@
-function Home() {
+import { ReactTyped } from "react-typed";
 
-    return(   
-    <section id="home" className="py-10 text-center">
-        <h1>Olá, sou o Victor</h1>
-        <p className="text-gray-400">Sou um programador apaixonado por programação back-end, em busca de aprender cada dia mais sobre programação de alto desempenho. | Atualmente estudando C++</p>
-    </section> 
-    );
-  }
+export const Home = () => {
+  return (
+    <section
+      id="home"
+      className="min-h-screen w-full bg-[#272525] flex items-center justify-center"
+    >
+      <div className="max-w-[1000px] px-8 flex flex-col items-start justify-center h-full">
+        <p className="font-mono text-white">Olá, meu nome é</p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-white">Victor</h1>
+        <h2 className="text-4xl sm:text-7xl font-bold text-gray">
+          <ReactTyped
+            strings={[
+              "Back-End Developer.",
+              "A.",
+              "texto genérico.",
+            ]}
+            typeSpeed={110}
+            backSpeed={55}
+            loop
+          />
+        </h2>
+        <p className="text-gray-400 py-4 max-w-[700px] font-mono">
+          Sou um programador apaixonado por programação back-end, em busca de
+          aprender cada dia mais sobre programação de alto desempenho. |
+          Atualmente estudando C++
+        </p>
+      </div>
+    </section>
+  );
+};
 
-  export default Home;
+export default Home;
